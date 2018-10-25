58cb12eb614103cda91b599730e6887f41c32e4d
sivanZhang <32605858+sivanZhang@users.noreply.github.com>
2018-10-25 19:32:05 +0800

    inbox样式

--- src/components/inbox/inbox.vue
+++ src/components/inbox/inbox.vue
@@ -7,26 +7,51 @@
                 <i class="fa fa-search" aria-hidden="true"></i>
             </div>
         </h4>
+        <section>
+            <div>
+                <img src="" alt="">
+            </div>
+            <div>
+                <div class="name">
+                    Chancy
+                    <div class="time">
+
+                    </div>
+                </div>
+                <div class="msg">
+
+                </div>
+                <div class="recent-chat">
+
+                </div>
+            </div>
+        </section>
         <footer-menu></footer-menu>
     </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
 <style lang="less" scoped>
-h4{
-    display: flex;
-    justify-content: space-between;
-    text-align: left;
-    div{
-        text-align: right;
-        font-size: 14px;
-        i:last-type-of{
-            
-        }
+.flex {
+  display: flex;
+  justify-content: space-between;
+}
+h4 {
+  .flex;
+  text-align: left;
+  div {
+    text-align: right;
+    font-size: 14px;
+    .fa {
+      &:last-of-type {
+        margin-left: 30px;
+      }
     }
+  }
+}
+section {
+    .flex;
 }
 </style>
 
